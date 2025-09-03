22dce35bb96ffe2f8ad946ca90ee59fd0816e1e6
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2025-09-03 23:53:48 +0530

    Merge Sorted Array

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -1,6 +1,20 @@
 public class RemoveDuplicate {
     public static void main(String[] args) {
-        System.out.println("conding interview questions");
+       int num[]={0,1,1,1,2,2,3};
+       System.out.println(mergeSortedArray(num));
+    }
+    public static int mergeSortedArray(int []nums){
+        int i=0; 
+        for(int j=1;j<nums.length; j++){
+            if(nums[i]< nums[j]){
+
+                int temp=nums[i+1];
+                nums[i+1]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+        }
+        return i+1;
     }
 
 }
