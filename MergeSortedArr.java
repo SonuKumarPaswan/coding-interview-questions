b0742d521344904a9fd5771a77939a0107c04b7d
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2025-09-06 23:55:36 +0530

    Merger Sorted two Array

--- MergeSortedArr.java
+++ MergeSortedArr.java
@@ -1,8 +1,37 @@
+import java.util.Arrays;
+
 public class MergeSortedArr {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
 
     public static void main(String[] args) {
-       int nums1[]={1,2,3,0,0,0};
-       int nums2[]={2,3,5};
-       
+        int[] nums1 = { 1, 3, 5, 0, 0, 0 };
+        int m = 3;
+        int[] nums2 = { 2, 4, 6 };
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        System.out.println("Merged Array: " + Arrays.toString(nums1));
     }
 }
